c886996959c445a73f6229c93c501f022fbe2dd8
Departamento Desenvolvimento <137066753+nunesempresarial@users.noreply.github.com>
2024-04-04 15:02:32 -0300

    Att

---
+++ organo/src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import "./Colaborador.css"
+
+const Colaborador = () => {
+    return (
+        <div>
+            <div>
+                <img src="https://github.com/andrlukk.png" alt="Lucas Andrade" />
+            </div>
+            <div>
+                <h4>Lucas Andrade</h4>
+                <h5>Estagiário</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
